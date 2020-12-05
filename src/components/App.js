aa985231f385644ca85f54f2cb0b0f78930ea194
HariKishore7 <harikishoret99@gmail.com>
2020-12-05 17:17:25 +0530

    Chnages made

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,10 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-
+import React from 'react';
+export default class App extends React.Component{
+    render(){
+        return(
+            <>
+                <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+            </>
+        );
+    }
+}
