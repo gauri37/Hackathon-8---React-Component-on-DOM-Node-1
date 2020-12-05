667dda989fb31003852d90193dd95fa6505d36a0
HariKishore7 <harikishoret99@gmail.com>
2020-12-05 17:22:36 +0530

    Changes made again

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ export default class App extends React.Component{
     render(){
         return(
             <>
-                <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+                <p id="root">Now I can render any React component on any DOM node I want using ReactDOM.render</p>
             </>
         );
     }
