7a0914150ed4fe35dc292803a202e3b63c3c06d8
gauri37 <73545480+gauri37@users.noreply.github.com>
2020-12-05 22:05:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
-class App extends React.Component{
-    render(){
-        return(
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        );
-    }
+function App() {
+    return (
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    );
 }
 export default App;
+
